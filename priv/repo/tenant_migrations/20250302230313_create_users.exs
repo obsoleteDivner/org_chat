d9468becab68024e80800a138769c8b6f1139a2d
Anatolii Voloshyn <anatolii.voloshyn@marmend.com>
2025-03-03 01:05:17 +0200

    added user tenant migration and updated company migration

---
+++ priv/repo/tenant_migrations/20250302230313_create_users.exs
@@ -0,0 +1,19 @@
+defmodule OrgChat.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :uuid, :uuid, primary_key: true
+      add :email, :string, null: false
+      add :first_name, :string
+      add :last_name, :string
+      add :role, :string, null: false, default: "user"
+      add :password, :string, null: false
+      add :password_hash, :string, null: false
+
+      timestamps()
+    end
+    create unique_index(:users, [:uuid])
+    create unique_index(:users, [:email])
+  end
+end
