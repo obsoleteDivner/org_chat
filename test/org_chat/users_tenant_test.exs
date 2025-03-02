5fe9a6cbe54bb340ae6bcd3e926ba3d185fb591f
Anatolii Voloshyn <anatolii.voloshyn@marmend.com>
2025-03-03 01:46:33 +0200

    Added user_fixture

--- test/org_chat/users_tenant_test.exs
+++ test/org_chat/users_tenant_test.exs
@@ -1,5 +1,7 @@
 defmodule OrgChat.UsersTenantTest do
   use OrgChat.DataCase, async: false
+  import OrgChat.UserFixtures  
+
   alias Ecto.Adapters.SQL.Sandbox
   alias OrgChat.Users
   alias OrgChat.Users.User
@@ -26,23 +28,8 @@ defmodule OrgChat.UsersTenantTest do
     Triplex.migrate(@tenant, Repo)
   end
 
-  def user_fixture(attrs \\ %{}, tenant \\ @tenant) do
-    valid_attrs = %{
-      "email" => "user#{System.unique_integer([:positive])}@example.com",
-      "password" => "Secret123!",
-      "first_name" => "Test",
-      "last_name" => "User",
-      "role" => "user",
-      "properties" => %{}
-    }
-
-    merged_attrs = Map.merge(valid_attrs, attrs)
-    {:ok, user} = Users.create_user(merged_attrs, tenant)
-    user
-  end
-
   describe "list_users/1" do
-    test "повертає список користувачів для заданої компанії" do
+    test "returns a list of users for a given company" do
       user = user_fixture(%{}, @tenant)
       users = Users.list_users(@tenant)
       assert user in users
@@ -50,7 +37,7 @@ defmodule OrgChat.UsersTenantTest do
   end
 
   describe "get_user!/2" do
-    test "повертає користувача за uuid для заданої компанії" do
+    test "returns the user by uuid for a given company" do
       user = user_fixture(%{}, @tenant)
       fetched = Users.get_user!(user.uuid, @tenant)
       assert fetched.uuid == user.uuid
@@ -58,7 +45,7 @@ defmodule OrgChat.UsersTenantTest do
   end
 
   describe "get_user_by_email/2" do
-    test "повертає користувача за email для заданої компанії" do
+    test "returns the user by email for a given company" do
       user = user_fixture(%{}, @tenant)
       fetched = Users.get_user_by_email(user.email, @tenant)
       assert fetched.email == user.email
@@ -66,7 +53,7 @@ defmodule OrgChat.UsersTenantTest do
   end
 
   describe "create_user/2" do
-    test "створює користувача з валідними даними" do
+    test "creates a user with valid data" do
       valid_attrs = %{
         "email" => "newuser@example.com",
         "password" => "Secret123!",
@@ -81,21 +68,21 @@ defmodule OrgChat.UsersTenantTest do
       assert is_binary(user.password_hash)
     end
 
-    test "повертає помилку при створенні користувача з невалідними даними" do
+    test "returns an error when creating a user with invalid data" do
       invalid_attrs = %{"email" => nil, "password" => nil}
       assert {:error, %Ecto.Changeset{}} = Users.create_user(invalid_attrs, @tenant)
     end
   end
 
   describe "update_user/3" do
-    test "оновлює користувача з валідними даними" do
+    test "updates a user with valid data" do
       user = user_fixture(%{}, @tenant)
       update_attrs = %{"first_name" => "Updated"}
       assert {:ok, %User{} = updated_user} = Users.update_user(user, update_attrs, @tenant)
       assert updated_user.first_name == "Updated"
     end
 
-    test "повертає помилку при оновленні користувача з невалідними даними" do
+    test "returns an error when updating a user with invalid data" do
       user = user_fixture(%{}, @tenant)
       invalid_attrs = %{"email" => nil}
       assert {:error, %Ecto.Changeset{}} = Users.update_user(user, invalid_attrs, @tenant)
@@ -103,7 +90,7 @@ defmodule OrgChat.UsersTenantTest do
   end
 
   describe "delete_user/2" do
-    test "видаляє користувача для заданої компанії" do
+    test "deletes a user for a given company" do
       user = user_fixture(%{}, @tenant)
       assert {:ok, %User{}} = Users.delete_user(user, @tenant)
 
@@ -114,7 +101,7 @@ defmodule OrgChat.UsersTenantTest do
   end
 
   describe "change_user/2" do
-    test "повертає changeset" do
+    test "returns a changeset" do
       user = user_fixture(%{}, @tenant)
       changeset = Users.change_user(user, %{}, @tenant)
       assert %Ecto.Changeset{} = changeset
@@ -122,21 +109,21 @@ defmodule OrgChat.UsersTenantTest do
   end
 
   describe "authenticate_user/3" do
-    test "повертає користувача з валідними даними" do
+    test "returns the user with valid credentials" do
       password = "Secret123!"
       user = user_fixture(%{"password" => password}, @tenant)
       assert {:ok, auth_user} = Users.authenticate_user(user.email, password, @tenant)
       assert auth_user.uuid == user.uuid
     end
 
-    test "повертає помилку при невірному паролі" do
+    test "returns an error when the password is incorrect" do
       user = user_fixture(%{}, @tenant)
 
       assert {:error, :invalid_password} =
                Users.authenticate_user(user.email, "wrongpass", @tenant)
     end
 
-    test "повертає помилку, якщо користувача не існує" do
+    test "returns an error when the user does not exist" do
       assert {:error, :user_not_found} =
                Users.authenticate_user("nonexistent@example.com", "Secret123!", @tenant)
     end
