f8b5e902dd61d7793b71d2ae2eb5e07514edcb2f
Anatolii Voloshyn <anatolii.voloshyn@marmend.com>
2025-03-02 23:01:06 +0200

    created project

---
+++ priv/repo/migrations/20250302211240_create_companies.exs
@@ -0,0 +1,17 @@
+defmodule CorporateChat.Repo.Migrations.CreateCompanies do
+  use Ecto.Migration
+
+  def change do
+    create table(:companies, primary_key: false) do
+      add :uuid, :uuid, primary_key: true, default: fragment("gen_random_uuid()")
+      add :name, :string, null: false, unique: true
+      add :full_name, :string, null: false, unique: true
+      add :company_avatar, :string
+      add :locale, :string, default: "en"
+
+      timestamps()
+    end
+
+    create unique_index(:companies, [:name])
+  end
+end
